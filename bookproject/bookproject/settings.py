274bdad649196efbacd766a6fb0e072def4afdfe
ike-burn <ike.burn.energy.0907@i.softbank.jp>
2026-04-01 18:30:51 +0900

    ログイン機能の実装

--- bookproject/bookproject/settings.py
+++ bookproject/bookproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bookproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR.parent / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'list-book'
