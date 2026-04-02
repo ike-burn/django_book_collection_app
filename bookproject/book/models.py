0688dd7fb2605fac0380fe0e2da2753becd69636
ike-burn <ike.burn.energy.0907@i.softbank.jp>
2026-04-02 15:54:59 +0900

    レビュー機能の実装

--- bookproject/book/models.py
+++ bookproject/book/models.py
@@ -1,17 +1,29 @@
 from django.db import models
+from .consts import MAX_RATE
 
 # 「タプル（順番付きで値を並べる）」、choiesに渡すための候補一覧
 CATEGORY = (('business', 'ビジネス'), ('life', '生活'), ('other', 'その他'))
+RATE_CHOICES = [(x, str(x)) for x in range(0, MAX_RATE + 1)]
 
 class Book(models.Model):
-        # 「フィールド」とは、モデルの中に書く「データの項目」。タイトル、説明や本文、分類
-        title = models.CharField(max_length=100) # 本のタイトルを入れる項目
-        text = models.TextField() # 本の説明や本文を入れる項目
-        category = models.CharField(
-            max_length=100,
-            choices = CATEGORY
-        ) # 本の分類を入れる項目
+    # 「フィールド」とは、モデルの中に書く「データの項目」。タイトル、説明や本文、分類
+    title = models.CharField(max_length=100) # 本のタイトルを入れる項目
+    text = models.TextField() # 本の説明や本文を入れる項目
+    category = models.CharField(
+        max_length=100,
+        choices = CATEGORY
+    ) # 本の分類を入れる項目
 
-        # 「__str__」は特殊メソッド（オブジェクトの文字列表現を返す）
-        def __str__(self):
-            return self.title
+    # 「__str__」は特殊メソッド（オブジェクトの文字列表現を返す）
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    rate = models.IntegerField(choices=RATE_CHOICES)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- bookproject/book/views.py
+++ bookproject/book/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.urls import reverse,reverse_lazy
 from django.views.generic import (
 	ListView,
 	DetailView,
@@ -7,7 +7,7 @@ from django.views.generic import (
 	DeleteView,
 	UpdateView,
 )
-from .models import Book
+from .models import Book, Review
 
 class ListBookView(ListView):
     template_name = 'book/book_list.html'
@@ -47,3 +47,33 @@ def index_view(request):
     # ３つ目の引数には「context」を指定する。辞書型データ（左がkey、右がvalue）である。keyを呼び出すことでvalueを呼び出す。
     # 「 {'object_list': object_list}」はBookモデルの全データを「object_list」（右）で呼び出せるようにしている。
     return render(request, 'book/index.html', {'object_list': object_list})
+
+class CreateReviewView(CreateView):
+    model = Review
+    fields = ('book', 'title', 'text', 'rate')
+    template_name = 'book/review_form.html'
+
+    # 「context」は辞書型のデータ。
+    # 「**kwargs」はキーワード引数。「urls.py」の「path('book/<int:book_id>/review/', views.CreateReviewView.as_view(), name='review'),」の<int:book_id>が「view」に渡される。
+    def get_context_data(self, **kwargs):
+        # 「super()」は継承元（CreateViewクラス)のメソッドを呼び出すことを意味する。
+        # すなわち、「get_context_data」というメソッドを上書きするために「CreateViewクラス（親クラス）」の「get_context_data」を呼び出す。
+        context = super().get_context_data(**kwargs)
+        # 「context['book'] 」は、辞書型のデータ（context）にオブジェクトを追加することを意味し、右側にどのオブジェクトを追加するかを記述する。
+        # また、「Book.objects.get」という部分は、Bookモデルの全てのデータから「getメソッド」を指定したデータを取得するために記述する。
+        # 「kwargs['book_id']」は、URLの中で定義した<int:book_id>に対応し、入力された数字に対応した書籍のデータを取得することが出来る。
+        context['book'] = Book.objects.get(pk=self.kwargs['book_id'])
+        return context
+
+	# 「form_valid」はフォームが送信され、その入力内容に間違いが無い場合に、データが保存される前に呼び出されるメソッド。
+    def form_valid(self, form):
+        # 以下の記述は、「form（Formクラス）」の「instance（フォームが作成された時に作られるデータ」に「user」という属性でデータを追加することを意味する。
+        # 「self.request.user」は、ユーザーがログインしている場合の「request」オブジェクトに入っている「user」の情報（ログインしているユーザーの情報）を意味する。
+        form.instance.user = self.request.user
+
+        return super().form_valid(form)
+
+	# クラス変数にコードを書く時は「reverse_lazy」を使用していたが、今回は「get_success_url」のメソッドの中にコードを書くため、「reverse」を使う。
+    def get_success_url(self):
+        # 「kwargs={'pk': self.object.book.id}」の形でキーワード引数に書籍のidの番号を渡す。
+        return reverse('detail-book', kwargs={'pk': self.object.book.id})
